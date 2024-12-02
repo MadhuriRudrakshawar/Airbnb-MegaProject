2973aca87d7763e8cc4a9d724f582a8407a60f12
Madhuri Rudrakshawar <madhuridhupdhare@gmail.com>
2024-12-02 14:01:58 +0000

    Frist commit

---
+++ init/index.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const initData = require("./data");
+const Listing = require("../models/listing");
+
+
+main()
+.then(()=> {
+    console.log("Connected to db");
+})
+.catch((err)=> {
+    console.log(err)
+});
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/wonderlust');
+}  
+
+const initDB = async() =>{
+   await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
+    console.log("data was initialised");
+};
+
+initDB();
